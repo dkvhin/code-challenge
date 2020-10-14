94142e0bf53c31289fa9e1e04b722e1fab505dc3
ervin.musngi <ervinm@d7softwaresolutions.com>
2020-10-14 14:00:23 +0800

    code update

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
         <title>Code Challenge</title>
     </head>
     <body>
+        <!--
+            I can make this code more shorter / less code,
+            but I think readability for other developers is more important
+        -->
         <ul style="list-style:none;">
             <?php for($i = 1; $i <= 100; $i++): ?>
                 <?php
